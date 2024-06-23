ba5951e0630e79c67832e881046daf967127ac34
Vedran-Josipovic <vedranjosipovic.posao@gmail.com>
2024-06-23 11:04:08 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module javafx.prod.architecturemanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens javafx.prod.architecturemanager to javafx.fxml;
+    exports javafx.prod.architecturemanager;
+}
